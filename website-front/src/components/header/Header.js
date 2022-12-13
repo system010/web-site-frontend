75689f7546b37fb025cb6df2e44bcf9a119e06c9
mina <mena24344@gmail.com>
2022-12-14 00:49:43 +0200

    header commit

---
+++ website-front/src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Header.css'
+
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='contain'>
+                <img className='image' src='sport_free-file.png' >
+                </img>
+
+            </div>
+        </div >
+    )
+}
+
+export default Header
