00479ddb6a6dbbb0a1d9090f4ebef012bc0154c2
paulaashraf77p <paulaashraf77p@gmail.com>
2022-12-15 21:36:13 +0200

    Edit Footer

--- website-front/src/components/header/footer/Footer.js
+++ website-front/src/components/header/footer/Footer.js
@@ -7,40 +7,40 @@ export const Footer = () => {
             <div className='container '>
                 <div class="row ">
                     <div class="col-md-4">
-                        <h3>شركتنا</h3>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-phone "></i> التليفون: + 01007243297 </span></a></li>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-mobile "></i>  الموبيل: + 01004563097</span></a></li>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="far fa-envelope"></i>  الأيميل:
+                        <h3>Contact With Vamos</h3>
+                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-phone "></i> Phone: + 01007243297 </span></a></li>
+                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-mobile "></i>  Phone: + 01004563097</span></a></li>
+                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="far fa-envelope"></i>  E-Mail:
                             info@vamos.com</span></a></li>
 
 
 
                     </div>
                     <div class="col-md-3">
-                        <h3>معلومات</h3>
+                        <h3>Details</h3>
                         <ul class="list-unstyled ">
-                            <li><a href="about-us.php ">عن متجرنا</a></li>
-                            <li><a href="allproducts.php">منتجاتنا</a></li>
+                            <li><a href="about-us.php ">About Our Store </a></li>
+                            <li><a href="allproducts.php">Our Collection</a></li>
 
-                            <li><a href="contact-us.php">ارسل لنا</a></li>
+                            <li><a href="contact-us.php">Contact Us </a></li>
                         </ul>
                     </div>
 
                     <div class="col-md-3">
-                        <h3>حسابى</h3>
+                        <h3>My Account</h3>
                         <ul class="list-unstyled ">
-                            <li><a href="login.php"><i class="fas fa-key"></i>دخول</a></li>
-                            <li><a href="regiester.php"> <i class="fas fa-lock"></i>تسجيل دخول</a>
+                            <li><a href="login.php"><i class="fas fa-key"></i>Login</a></li>
+                            <li><a href="regiester.php"> <i class="fas fa-lock"></i> Register</a>
                             </li>
                         </ul>
                     </div>
                     <div class="col-md-2">
-                        <h3>تابعنا على</h3>
+                        <h3> Contact With Vamos</h3>
                         <ul class="list-unstyled ">
-                            <li><a href="https://www.facebook.com/CurvaEgypt/" target="_blank"><i class="fab fa-facebook-f"></i> فيس بوك</a></li>
-                            <li><a href="https://twitter.com/CurvaEgypt " target="_blank"><i class="fab fa-twitter"></i> تويتر</a></li>
-                            <li><a href="https://www.instagram.com/curvaegypt/" target="_blank"><i class="fab fa-instagram"></i> إنستغرام</a></li>
-                            <li><a href="linkedin" target="_blank"><i class="fab fa-linkedin"></i> لينكد إن</a></li>
+                            <li><a href="https://www.facebook.com/CurvaEgypt/" target="_blank"><i class="fab fa-facebook-f"></i>  Face Book</a></li>
+                            <li><a href="https://twitter.com/CurvaEgypt " target="_blank"><i class="fab fa-twitter"></i> Twitter</a></li>
+                            <li><a href="https://www.instagram.com/curvaegypt/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+                            <li><a href="linkedin" target="_blank"><i class="fab fa-linkedin"></i>  LinkedIn</a></li>
                         </ul>
                     </div>
                 </div>
