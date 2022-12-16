879fede22d170613eadd74a31f97da6ae36ec209
mina <mena24344@gmail.com>
2022-12-16 02:06:30 +0200

    footer commit

--- website-front/src/components/header/footer/Footer.js
+++ website-front/src/components/header/footer/Footer.js
@@ -6,15 +6,22 @@ export const Footer = () => {
         <div className='footer'>
             <div className='container '>
                 <div class="row ">
-                    <div class="col-md-4">
+                    <div class="col-4">
                         <h3>شركتنا</h3>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-phone "></i> التليفون: + 01007243297 </span></a></li>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="fa fa-mobile "></i>  الموبيل: + 01004563097</span></a></li>
-                        <li><a title="Home " itemprop="url "><span itemprop="name "><i class="far fa-envelope"></i>  الأيميل:
-                            info@vamos.com</span></a></li>
-
-
-
+                        <p className='phone'>
+                            التليفون : 01007243297
+                        </p>
+                        <p className='phone'>
+                            التليفون : 01007243297
+                        </p>
+                        <p className='phone'>
+                            التليفون : 01007243297
+                        </p>
+                        <ul className='email'>
+                            <il >
+                                info@vamos.com : الايميل
+                            </il>
+                        </ul>
                     </div>
                     <div class="col-md-3">
                         <h3>معلومات</h3>
@@ -26,7 +33,7 @@ export const Footer = () => {
                         </ul>
                     </div>
 
-                    <div class="col-md-3">
+                    <div class="">
                         <h3>حسابى</h3>
                         <ul class="list-unstyled ">
                             <li><a href="login.php"><i class="fas fa-key"></i>دخول</a></li>
@@ -40,7 +47,6 @@ export const Footer = () => {
                             <li><a href="https://www.facebook.com/CurvaEgypt/" target="_blank"><i class="fab fa-facebook-f"></i> فيس بوك</a></li>
                             <li><a href="https://twitter.com/CurvaEgypt " target="_blank"><i class="fab fa-twitter"></i> تويتر</a></li>
                             <li><a href="https://www.instagram.com/curvaegypt/" target="_blank"><i class="fab fa-instagram"></i> إنستغرام</a></li>
-                            <li><a href="linkedin" target="_blank"><i class="fab fa-linkedin"></i> لينكد إن</a></li>
                         </ul>
                     </div>
                 </div>
