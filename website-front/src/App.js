06f81274329531815014534a4b4e1ccab8f333f1
paulaashraf77p <paulaashraf77p@gmail.com>
2022-12-19 13:26:52 +0200

    photo commit

--- website-front/src/App.js
+++ website-front/src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css';
 import { Footer } from './components/header/footer/Footer';
 import Header from './components/header/Header';
-
+import MainPage from './components/header/footer/Mainpage/MainPage';
 function App() {
   return (
     <div>
       <Header />
+      <MainPage />
       <Footer />
     </div>
   );
