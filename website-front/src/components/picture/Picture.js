8a78073cf7d6193073963c48d8117051abfd2324
mina <mena24344@gmail.com>
2022-12-20 23:47:05 +0200

    picture main page commit

---
+++ website-front/src/components/picture/Picture.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Picture.css'
+export const Picture = () => {
+    return (
+        <div className='content'>
+            <div className='picture'>
+                <img className='pic' src='group-happy-fans-are-cheering-their-team-victory.jpg'></img>
+            </div>
+            <div className='container'>
+                <h1>Welcome in</h1>
+                <h1> Vamos Store</h1>
+            </div>
+        </div>
+
+    )
+}
