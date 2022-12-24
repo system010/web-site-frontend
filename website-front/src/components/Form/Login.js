f028f9e51344d3ee857d1039a22f6a7117c16e8b
mina <mena24344@gmail.com>
2022-12-24 22:13:15 +0200

    commit

--- website-front/src/components/Form/Login.js
+++ website-front/src/components/Form/Login.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
--- website-front/src/components/Form/Register.js
+++ website-front/src/components/Form/Register.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
