18732146960054973be02fcf53ccaf5d38b2b5f9
mina <mena24344@gmail.com>
2022-12-28 15:03:52 +0200

    changes

--- website-front/src/components/fifa/Fifaitem.js
+++ website-front/src/components/fifa/Fifaitem.js
@@ -6,6 +6,7 @@ function Fifaitem({ image, name, price }) {
             <div style={{ backgroundImage: `url(${image})` }}> </div>
             <h1> {name} </h1>
             <p> ج{price} </p>
+            <button className="button" > buy </button>
         </div>
     );
 }
